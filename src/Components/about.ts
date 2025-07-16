5ae5e3d2f1de060d1c62fbf93a58d9f67cf12d85
arcadyrajeev <bitmasteratj@gmail.com>
2025-07-16 21:17:14 +0530

    section added

--- src/Components/about.ts
+++ src/Components/about.ts
@@ -1,11 +1,24 @@
 export function About() {
   return `
-    <header class="about" id="about">
-      <h1>Lunaria Photography</h1>
-      <nav>
-        <a href="#gallery">Gallery</a>
-        <a href="#contact">Contact</a>
-      </nav>
-    </header>
+    <div class="about" id="about">
+    <h1>About</h1>
+    <div class="content">
+            <div class="image-block">
+        <img>
+      </div>
+      <div class="text-block">
+      <p>
+        At Lunaria Photography, we believe in capturing more than just moments — we capture stories, emotions, and the quiet magic found in everyday beauty. Inspired by the delicate and luminous Lunaria flower, our work is rooted in elegance, authenticity, and timeless expression.
+      </p>
+      <p>
+        Founded with a passion for visual storytelling, Lunaria blends artistic vision with technical precision to create imagery that resonates deeply. Whether it's the intimate glow of a wedding, the quiet strength of a portrait, or the vivid pulse of a brand, our goal is to document what makes your story uniquely yours.
+      </p>
+      <p>
+        We don’t just take photographs — we create heirlooms. Pieces of memory you’ll return to again and again.
+      </p>
+      </div>
+    </div>
+      
+    </div>
   `;
 }
